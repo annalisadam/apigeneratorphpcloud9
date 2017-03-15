cca7b892cce16a02952b477248fc79e2fe0cfc7d
annalisadamore91 <annalisadamore91@libero.it>
2017-03-15 23:34:57 +0000

    aggiunta dell'index.php al file su icloud9

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP</title>
+        <meta charset="utf-8";>
+    </head>
+    <body>
+        <p>
+            <?php 
+            echo "Hello World;"
+            ?>
+        </p>
+    </body>
+</html>
